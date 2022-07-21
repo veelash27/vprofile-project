39a531b089743f2d9d3c9d4e82b1a55a92ed4be5
veelash27 <64749834+veelash27@users.noreply.github.com>
2022-07-21 18:52:53 +0530

    Update rabbitmq.sh

--- userdata/rabbitmq.sh
+++ userdata/rabbitmq.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 sudo amazon-linux-extras install epel -y
 sudo yum update -y
-sudo yum install wget -y
 cd /tmp/
-wget http://packages.erlang-solutions.com/erlang-solutions-2.0-1.noarch.rpm
-sudo rpm -Uvh erlang-solutions-2.0-1.noarch.rpm
-sudo yum -y install erlang socat
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
 sudo yum install rabbitmq-server -y
-sudo systemctl start rabbitmq-server
+sudo rabbitmq-plugins enable rabbitmq_management
 sudo systemctl enable rabbitmq-server
+sudo systemctl start rabbitmq-server
 sudo systemctl status rabbitmq-server
 sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
 sudo rabbitmqctl add_user test test
